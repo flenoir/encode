3ab922152ffcf74363cc28d179cec36a99b02d67
fabien lenoir <flenoir@gmail.com>
2016-05-25 09:57:44 +0200

    ajout Materialize et moment.js

--- client/encode.js
+++ client/encode.js
@@ -1,5 +1,6 @@
 
   Template.inputFile.events({
+
     'click .btn': function (evt) {
       evt.preventDefault();
       var input = $('.input_file').val(); // récupération avec Jquery de la valeur de l'input
@@ -8,7 +9,7 @@
       var inputok = input.replace("C:\\fakepath\\", "C:\\bin\\");
       var inputClean  = input.replace("C:\\fakepath\\", "");
 
-      var searchedId = files.insert({name: inputok, status: 'inital', createdAt: new Date()});
+      var searchedId = files.insert({name: inputok, status: 'inital', createdAt: moment().format("Do MMMM YYYY, h:mm:ss a")});
 
        var inputId = searchedId;
 
@@ -21,6 +22,7 @@
        };
 
        console.log(inputObject.path);
+       console.log(inputObject.aspectRatio);
        console.log(inputObject.codec);
        console.log(inputObject.name);
 
@@ -36,3 +38,15 @@
   });
 
 
+ Template.show_file.helpers({
+    file: function () {
+      var search = files.find();
+      return search;
+      }
+  });
+
+
+// Template à ajouter pour permettre le rendu des input "select" de Materialize
+ Template.show_file.onRendered(function() { 
+    $('select').material_select();
+});
